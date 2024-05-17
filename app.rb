f3c58cc490a376fc36bfcf0cf5e999f0732f645f
Ryan <ryananthonyrichardson@gmail.com>
2024-05-16 23:04:11 -0700

    feat: add more input control and feather images

--- app.rb
+++ app.rb
@@ -23,13 +23,75 @@ class App < Sinatra::Base
   end
 
   def self.sketch_prompt(detail)
-    "Please create a minimalist, black and white pencil " \
+    "Please create a minimalist, campy, black and white pencil " \
     "drawing, of \"#{detail}\". " \
     "This image is for someone who CANNOT read."
   end
 
-  def self.beat_count
+  def self.prompt_count
     2
+  end
+
+  def self.upscale
+    Dir.glob("#{App.root}/*.png").each do |path_to_image|
+      next if path_to_image.include? "--upscaled"
+
+      parts = path_to_image.split('.')
+      output = "#{parts[0]}-upscaled.#{parts[1]}"
+
+      puts "Upscaling to #{output}"
+      system(
+        "./realesrgan-ncnn-vulkan " \
+        "-s 3 -v " \
+        "-i #{path_to_image} " \
+        "-o #{parts[0]}--upscaled.#{parts[1]}"
+      )
+
+      File.delete(path_to_image) if File.exist? path_to_image
+    end
+  end
+
+  def self.feather
+    Dir.glob("#{App.root}/*.png").each do |path_to_image|
+      parts = path_to_image.split('.')
+      output = "#{parts[0]}--feathered.#{parts[1]}"
+
+      puts "Feathering #{output}"
+      system(
+        "convert #{path_to_image} ./bgw_edge.png -alpha off " \
+        "-compose CopyOpacity " \
+        "-composite #{output}"
+      )
+
+      File.delete(path_to_image) if File.exist? path_to_image
+    end
+  end
+
+  def self.style
+    "<style> " \
+      "label { " \
+        "margin-right: 8px; " \
+        "margin-top: 4px; " \
+        "display: inline-block; } " \
+    "</style>"
+  end
+
+  def self.form
+    "<form action='/' method='post' \
+      enctype='multipart/form-data'>" \
+      "<p>Upload file (must be 25mb or less)</p>" \
+      "<label for='context'>Context</label>" \
+      "<input type='text' name='context' /><br>" \
+      "<label for='image_model'>Image Model</label>" \
+      "<select name='image_model' /> " \
+      "<option value='dall-e-2'>DALL-E 2</option>" \
+      "<option value='dall-e-3' selected='selected'>" \
+      "DALL-E 3</option>" \
+      "</select><br>" \
+      "<label for='audio'>Audio File</label>" \
+      "<input type='file' name='audio' accept='.mp3,audio/*' />" \
+      "<br><br><input type='submit' value='Generate' />" \
+    "</form>"
   end
 
   def self.today
@@ -45,35 +107,22 @@ class App < Sinatra::Base
   end
 
   get '/upscale' do
-    Dir.glob("#{App.root}/*.png").each do |path_to_image|
-      parts = path_to_image.split('.')
-      output = "#{parts[0]}-upscaled.#{parts[1]}"
+    App.upscale
+  end
 
-      puts "Upscaling to #{output}"
-      system(
-        "cd upscaler && ./realesrgan-ncnn-vulkan " \
-        "-s 3 -v " \
-        "-i #{path_to_image} " \
-        "-o #{parts[0]}--upscaled.#{parts[1]}"
-      )
-    end
+  get '/feather' do
+    App.feather
   end
 
   get '/' do
-    "<form action='/' method='post' \
-      enctype='multipart/form-data'>" \
-      "<p>Upload file (must be 25mb or less)</p>" \
-      "<input type='text' name='context' /><br>" \
-      "<input type='file' name='audio' accept='.mp3,audio/*' />" \
-      "<br><input type='submit' value='Upload Audio' />" \
-    "</form>"
+    App.style + App.form
   end
 
   # Takes an audio file and asks OpenAI to do the following:
   #
   #   - Determine Transcription
-  #   - Generate a number of "Beats" from the Transcription
-  #   - Generate an image for each Beat
+  #   - Generate a number of "prompts" from the Transcription
+  #   - Generate an image for each Prompt
   #
   post '/' do
     begin
@@ -103,8 +152,8 @@ class App < Sinatra::Base
       end
       puts "Transcription Complete"
 
-      # Step 2: Convert the text to beats
-      unless @cache[:beats]
+      # Step 2: Convert the text to prompts
+      unless @cache[:prompts]
         step2_response = App.open_ai.chat(
           parameters: {
             model: "gpt-4o",
@@ -114,12 +163,13 @@ class App < Sinatra::Base
               role: "user",
               content: "Please consider this full text following " \
                 "the colon and concisely summarize 40 stand-alone " \
-                "'beats' (one short sentence each) from it." \
-                "#{' Keep in mind ' + context + ". " if context} " \
-                "Produce a JSON array of strings under the " \
-                "attribute name 'beats'. The total character " \
-                "count of all beats combined should be less than " \
-                "or equal to 600:\n\n#{@cache[:transcription]}."
+                "'prompts' optimized for DALL-E image generation." \
+                "They should be one short sentence each." \
+                "#{' Keep in mind ' + context + "." if context} " \
+                " Produce a JSON array of strings under the " \
+                "attribute name 'prompts'. The total character " \
+                "count of all prompts combined should be less than " \
+                "or equal to 1000:\n\n#{@cache[:transcription]}."
             }],
             temperature: 0.7,
           }
@@ -128,13 +178,13 @@ class App < Sinatra::Base
         content =
           step2_response["choices"].first["message"]["content"]
 
-        @cache[:beats] = JSON.parse(content)["beats"]
+        @cache[:prompts] = JSON.parse(content)["prompts"]
 
         App.write(@cache)
       end
-      puts "Beat Extraction Complete"
+      puts "Prompt Extraction Complete"
 
-      # Step 3: Summarize the beats
+      # Step 3: Summarize the prompts
       unless @cache[:summary]
         step3_response = App.open_ai.chat(
           parameters: {
@@ -144,7 +194,7 @@ class App < Sinatra::Base
             messages: [{
               role: "user",
               content: "Please summarize this text to 3 " \
-                "short sentences: #{@cache[:beats].join(' ')}. " \
+                "short sentences: #{@cache[:prompts].join(' ')}. " \
                 "#{'Keep in mind ' + context + ". " if context} " \
                 "Please produce the response as JSON under the " \
                 "attribute name 'summary'."
@@ -165,10 +215,10 @@ class App < Sinatra::Base
       @cache[:images] ||= []
       generated_images = Dir.glob("#{App.root}/*.png")
 
-      unless generated_images.length >= @cache[:beats].length
-        @cache[:beats].each_with_index do |beat, i|
+      unless generated_images.length >= @cache[:prompts].length
+        @cache[:prompts].each_with_index do |prompt, i|
           image_path = App.today + "/" + ("%03d" % (i+1)) +
-            "_" + beat.gsub(" ", "-").
+            "_" + prompt.gsub(" ", "-").
             gsub(/[^a-zA-Z\-]/, '') + ".png"
           puts "Generating #{image_path}"
 
@@ -176,11 +226,10 @@ class App < Sinatra::Base
 
           image_response = App.open_ai.images.generate(
             parameters: {
-              model: "dall-e-3", # 0.04 or 0.08 (HD)
-              # model: "dall-e-2",
+              model: params["image_model"],
               size: "1024x1024",
               quality: "standard",
-              prompt: App.sketch_prompt(beat)
+              prompt: App.sketch_prompt(prompt)
             }
           )
 
@@ -202,22 +251,7 @@ class App < Sinatra::Base
       end
 
       # Step 5: Upscale the images
-      Dir.glob("#{App.root}/*.png").each do |path_to_image|
-        next if path_to_image.include? "--upscaled"
-
-        parts = path_to_image.split('.')
-        output = "#{parts[0]}-upscaled.#{parts[1]}"
-
-        puts "Upscaling to #{output}"
-        system(
-          "cd upscaler && ./realesrgan-ncnn-vulkan " \
-          "-s 3 -v " \
-          "-i #{path_to_image} " \
-          "-o #{parts[0]}--upscaled.#{parts[1]}"
-        )
-
-        File.delete(path_to_image) if File.exist? path_to_image
-      end
+      App.upscale
     rescue StandardError => e
       byebug
     end
